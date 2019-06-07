84c6bf7c069b42b67cfbfca706f53ac52f737edc
Moreira RAFAEL <Moreira RAFAEL@DESKTOP-S5NO3R3>
2019-06-06 21:13:37 -0400

    06 06 2016

--- br.com.gobr/src/main/java/br/com/gobr/dao/PessoaJPA.java
+++ br.com.gobr/src/main/java/br/com/gobr/dao/PessoaJPA.java
@@ -1,7 +1,7 @@
 package br.com.gobr.dao;
 
 import javax.persistence.EntityManager;
-
+import org.hibernate.mapping.List;
 import br.com.gobr.model.Cliente;
 
 public class PessoaJPA {
@@ -12,7 +12,36 @@ public class PessoaJPA {
 		em = dao.getEntity();
 	}
 	
+	// Busca cliente por id no banco de dados
 	public Cliente getById(final int id) {
 		return em.find(Cliente.class, id);
 	}
+	
+	// Salvar cliente no banco de dados
+	public void save(Cliente c) {
+		em.getTransaction().begin();
+		em.persist(c);
+		em.getTransaction().commit();
+	}
+	
+	// Atualizar cliente no banco de dados
+	public void update(Cliente c) {
+		em.getTransaction().begin();
+		em.merge(c);
+		em.getTransaction().commit();
+	}
+	
+	// Remover cliente no banco de dados
+	public void delete(Cliente c) {
+		em.getTransaction().begin();
+		Cliente clienteRemover = getById(c.getIdPessoa());
+		em.remove(clienteRemover);
+		em.getTransaction().commit();
+	}
+	
+	// Consulta
+	public List<Cliente> getAll(){
+		return em.createQuery("FROM" + Cliente.class.getName()).getResultList();
+	}
+	
 }
