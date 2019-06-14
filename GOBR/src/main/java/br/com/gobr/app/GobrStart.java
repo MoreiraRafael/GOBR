c37c454495d92526d099bf0ffeb99ede62ede584
Moreira RAFAEL <Moreira RAFAEL@DESKTOP-S5NO3R3>
2019-06-13 21:27:35 -0400

    workspace

--- GOBR/src/main/java/br/com/gobr/app/GobrStart.java
+++ GOBR/src/main/java/br/com/gobr/app/GobrStart.java
@@ -3,19 +3,32 @@ package br.com.gobr.app;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import br.com.gobr.jpa.Conexao;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
-public class GobrStart {
+public class GobrStart extends Application {
 
 	// private static Logger LOG = Logger.getLogger(GobrStart.class);
 	@SuppressWarnings("unused")
-	private static final Logger logger = LogManager.getLogger(GobrStart.class);
+	private static final Logger LOG = LogManager.getLogger(GobrStart.class);
 
 	public static void main(String[] args) {
 
 		// LOG.info("Iniciando conexão");
-		Conexao conn = new Conexao();
-		conn.getEntity();
+		//Conexao conn = new Conexao();
+		//conn.getEntity();
+		launch(args);
+	}
 
+	@Override
+	public void start(Stage args) throws Exception {
+		// TODO Auto-generated method stub
+		Parent root = FXMLLoader.load(getClass().getResource("../view/Login.fxml"));
+		Scene scene = new Scene(root);
+		args.setScene(scene);
+		args.show();
 	}
 }
